267ce7939efc369142ceb9513c334f37e2612200
wangmj <wangmingjie20@outlook.com>
2017-06-06 09:43:53 +0800

    changed app.config-SPASitePath to  relative path

--- BOCTS.Client.SPA.Host/Startup.cs
+++ BOCTS.Client.SPA.Host/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.Owin.StaticFiles;
 using Owin;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +17,12 @@ namespace BOCTS.Client.SPA.Host
         public void Configuration(IAppBuilder app)
         {
             var root = FrameWork.EnvironmentData.ClientSettings.SPASitePath;
-
+            if(root.StartsWith(".\\")||root.StartsWith("..\\")||root.StartsWith("\\"))
+            {
+                Debug.Write(AppDomain.CurrentDomain.BaseDirectory);
+                root= Path.Combine(AppDomain.CurrentDomain.BaseDirectory, root);
+                Debug.Write(root);
+            }
             var fileSystem = new PhysicalFileSystem(root);
             var option = new FileServerOptions
             {
