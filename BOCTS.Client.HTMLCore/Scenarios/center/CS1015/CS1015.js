614322d393b98f46e6f282548def1f1fc7e88e43
sun1630 <sun1630@126.com>
2017-06-09 16:03:13 +0800

    案例拆分- 省市联动

--- BOCTS.Client.HTMLCore/Scenarios/center/CS1015/CS1015.js
+++ BOCTS.Client.HTMLCore/Scenarios/center/CS1015/CS1015.js
@@ -1,26 +1,16 @@
-﻿define(['activities/ShowScreenActivity', 'activities/DataMappingActivity', 'activities/ConnectServerActivity', 'activities/CallSubFlowActivity']
+﻿define(['activities/ShowScreenActivity',
+   'activities/CallSubFlowActivity']
     ,
-    function (showScreen, setDataAct, connectServer,  CallSubFlowActivity) {
+    function (showScreen,  CallSubFlowActivity) {
         return {
             $inputs: '*',
             $outputs: '*',
             $global: { sendData: null, showData: null },
             activities: {
-                //'readIDCard': wfjs.Activity({
-                //    activity: new readIDCard(),
-                //    $inputs: {
-                //    },
-                //    next: 'showAccount'
-                //}),
                 'showAccount': wfjs.Activity({
                     activity: new showScreen(),
                     $inputs: {
-                        Page: '"Scenarios/center/CS1015/CS1015_1"',
-                        //PageTimeOut: 60,     //int型 设置页面超时时间
-                        //DialogTimeOut: 10,   //int型 设置页面超时后对话框超时时间
-                        //DialogMessage: '"$rm.global.message.timeOutTips()"',  //string型 设置页面超时后对话框显示信息
-                        //ForPadMessage: '"$rm.global.message.mainViewTimeOut()"',  //string型 设置对话框超时后发Pad信息
-                    },
+                        Page: '"Scenarios/center/CS1015/CS1015_1"', },
                     next: 'showName'
                 }),
                 'CallSubFlowActivity1': wfjs.Activity({
@@ -32,54 +22,11 @@
                 'showName': wfjs.Activity({
                     activity: new showScreen(),
                     $inputs: {
-                        Page: '"Scenarios/center/CS1015/CS1015_2"',
+                        Page: '"Scenarios/center/CS1015/CS1015_4"',
                         ShowType: '"normal"',//normal model
-                        //PageTimeOut: 10,     //int型 设置页面超时时间
-                        //DialogTimeOut: 10,   //int型 设置页面超时后对话框超时时间
-                        //DialogMessage: '"$rm.global.message.timeOutTips()"',  //string型 设置页面超时后对话框显示信息
-                        //ForPadMessage: '"$rm.global.message.mainViewTimeOut()"',  //string型 设置对话框超时后发Pad信息
-                    },
+                         },
                     next: 'null'
                 }),
-                //'setdata': wfjs.Activity({
-                //    activity: new setDataAct(),
-                //    $inputs: {
-                //        Mapping: [
-                //            { source: "account", target: "acc" },
-                //            { source: "password", target: "pwd" },
-                //            { source: "name", target: "name" },
-                //            { source: "birthday", target: "birthday" }
-                //        ]
-                //    },
-                //    $outputs: { 'Result': 'sendData' },
-                //    next: 'setdata2'
-                //}),
-                //'setdata2': wfjs.Activity({
-                //    activity: new setDataAct(),
-                //    $inputs: {
-                //        Source: '"$dm"',
-                //        Target: 'this.sendData',
-                //        Mapping: [
-                //            { source: "account", target: "acc2" },
-                //            { source: "password", target: "pwd2" },
-                //            { source: "name", target: "name2" },
-                //            { source: "birthday", target: "birthday2" }
-                //        ]
-                //    },
-                //    $outputs: { 'Result': 'sendData' },
-                //    next: 'connectServer'
-                //}),
-                //'connectServer': wfjs.Activity({
-                //    activity: new connectServer(),
-                //    $inputs: {
-                //        Server: '"wf"',
-                //        Url: '"wf/request"',
-                //        Pars: { 'wf': 'CT1010' },
-                //        SendData: 'this.sendData'
-                //    },
-                //    $outputs: { 'Result': 'showData' },
-                //    next: 'showResult'
-                //}),
                 'showResult': wfjs.Activity({
                     activity: new showScreen(),
                     $inputs: {
@@ -98,17 +45,7 @@
                         ShowData: 'this.showData'
                     },
                     next: 'null'
-                }),
-                //'ErrorEndFlow': wfjs.Activity({
-                //    activity: new TradeSucceedActivity(),
-                //    next: null
-                //})
-                //'ErrorEndFlow': wfjs.Activity({
-                //    activity: new EndActivity(),
-                //    $inputs: { IsGoHome: true, HomePage: '"Shells/Counter/mainView"', },
-                //    $outputs: {},
-                //    next: null
-                //})
+                })
             }
         };
     });
