e046d348d7a757d6b6606790f810f6de5a8f1c60
Kevin <1985088337@qq.com>
2017-06-14 17:39:11 +0800

    autocomplete.js   自动完成扩展

---
+++ BOCTS.Client.HTMLCore/Scripts/widgets/autocomplete/autocomplete.js
@@ -0,0 +1,36 @@
+﻿define(["widgets/autocomplete/jquery.autocomplete"],
+    function() {
+
+        ko.bindingHandlers.autocomplete = {
+            init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+
+                var states = [
+                    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+                    'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+                    'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+                    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+                    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+                    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+                    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+                    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+                    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+                ];
+
+                var $element = $(element);
+
+                $element.autocomplete({
+                    source: [states]
+                });
+
+                var value = valueAccessor();
+                $element.off("keyup").on("keyup",
+                    function() {
+                        value($(this).val());
+                    });
+                
+            }
+
+        };
+
+
+    });
