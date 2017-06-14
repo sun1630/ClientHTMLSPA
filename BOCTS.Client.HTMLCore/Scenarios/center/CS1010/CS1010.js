9a54dcf423e2538a98cd254fb1ba7c174a95bccf
Kevin <1985088337@qq.com>
2017-06-14 09:40:22 +0800

    添加自动完成和树形测试页

---
+++ BOCTS.Client.HTMLCore/Scenarios/center/CS1010/CS1010.js
@@ -0,0 +1,29 @@
+﻿define(['activities/ShowScreenActivity', 'activities/DataMappingActivity', 'activities/ConnectServerActivity', 'activities/CallSubFlowActivity']
+    ,
+    function (showScreen, setDataAct, connectServer, CallSubFlowActivity) {
+        return {
+            $inputs: '*',
+            $outputs: '*',
+            $global: { sendData: null, showData: null },
+            activities: {
+                'showAccount': wfjs.Activity({
+                    activity: new showScreen(),
+                    $inputs: {
+                        Page: '"Scenarios/center/CS1010/CS1010_1"'
+                        //PageTimeOut: 60,     //int型 设置页面超时时间
+                        //DialogTimeOut: 10,   //int型 设置页面超时后对话框超时时间
+                        //DialogMessage: '"$rm.global.message.timeOutTips()"',  //string型 设置页面超时后对话框显示信息
+                        //ForPadMessage: '"$rm.global.message.mainViewTimeOut()"',  //string型 设置对话框超时后发Pad信息
+                    },
+                    next: 'null'
+                }),
+                'CallSubFlowActivity1': wfjs.Activity({
+                    activity: new CallSubFlowActivity(),
+                    $inputs: { SubChartName: '"CS1010/CS1010-11"' },
+                    $outputs: { '*': '*' },
+                    next: 'showName'
+                })
+         
+            }
+        };
+    });
---
+++ BOCTS.Client.HTMLCore/Scenarios/center/CS1010/CS1010_1.js
@@ -0,0 +1,46 @@
+﻿define(['jsRuntime/parManager', 'jsRuntime/resourceManager', 'jsRuntime/actionManager',
+    'widgets/popupSelect', 'widgets/maskInput', 'jsRuntime/workflowManager',
+     'jsRuntime/utility', 'udl/vmProvider', 'Config/fieldType'
+], function (pm, rm, am, popupSelect, maskInputs, wm, utility, vmp, cfg) {
+    var m = function (cx) {
+        var model = new vmp({
+            data: {
+                cx: cx,
+                rm: rm.global,
+                am: am.global,
+                wm: cx.wm,
+                dm: cx.dm,
+                instanceid: cx.instanceId,
+                tabId: cx.tabId,
+                //amount: {
+                //    value: 1000,
+                //    metadata: {
+                //        needObserve: true,
+                //        needShare: true,
+                //        inputMask: "YYYY-MM-DD",
+                //        inputComlplete: {
+                //            required: { params: true, message: "必填" }
+                //            //max: { params: 5, message: '最大值为5' },
+                //            //pattern: { params: '^[0-9]+\.{0,1}[0-9]{0,3}$', message: 'guifan' }
+                //            //readonly: true,
+                //        },
+                //        format: function (target, value) {
+                //            var newValue = parseFloat(target.rate.value()) * parseFloat(value);
+                //            return newValue;
+                //        }
+                //    }
+                //}
+            },
+            methods: {
+                //activate: function () {
+                //    alert();
+                //},
+                //compositionComplete: function () {
+                //    alert();
+                //}
+            }
+        }, cx);
+        return model;
+    }
+    return m;
+});
